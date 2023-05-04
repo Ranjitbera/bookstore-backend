bb35160aafb4c2fe613108c57bb5b1c61c84d2b3
Ranjitbera <124592068+Ranjitbera@users.noreply.github.com>
2023-05-04 12:05:52 +0530

    update conn.js

--- db/conn.js
+++ db/conn.js
@@ -2,7 +2,7 @@ const mongoose=require("mongoose")
 const dotenv = require('dotenv');
 dotenv.config();
 const connect = ()=>{
-    mongoose.connect(process.env.DATABASE
+    mongoose.connect("mongodb+srv://ranjitbera34567:bookstore@cluster0.j6emnwa.mongodb.net/"
         ).then(()=>{
             console.log("connected to database succesfully")
         })
